ba8a3fb6d388a108fb951693293e5dac9fcdf757
Nick Ratelle <nick.ratelle@gmail.com>
2020-12-06 14:52:12 -0500

    Day 6

---
+++ AoC2020/Day06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day06
+{
+    class Program
+    {
+        static IEnumerable<IList<string>> SplitOnBlankLines(string[] lines)
+        {
+            var accumulator = new List<string>();
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    yield return accumulator;
+                    accumulator = new List<string>();
+                }
+                else
+                {
+                    accumulator.Add(line);
+                }
+            }
+
+            if (accumulator.Count > 0)
+            {
+                yield return accumulator;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("input.txt");
+            var groups = SplitOnBlankLines(data).ToList();
+            
+            var groupCounts = groups.Select(x => string.Join("", x).Select(y => y).Distinct().Count());
+            Console.WriteLine($"Part I - {groupCounts.Sum()}");
+
+            var countAnsweredYesByAll = groups.Select(x => string.Join("", x).GroupBy(key => key, value => value).Where(y => y.Count() == x.Count).Count());
+            Console.WriteLine($"Part II - {countAnsweredYesByAll.Sum()}");
+        }
+    }
+}
